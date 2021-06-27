9cdf698d0b89fedefdcbae54cbe7d89c7702f216
Nagarani-Manokaran <85403326+Nagarani-Manokaran@users.noreply.github.com>
2021-06-27 04:11:15 -0700

    Add files via upload

---
+++ Footer.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+function Footer() {
+  return (
+    <footer>
+      <p> Copy right by Shape AI @{new Date().getFullYear()}</p>
+    </footer>
+  );
+}
+export default Footer;
---
+++ Info.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom";
+function Info() {
+  return (
+    <div ClassName="note">
+      <h1> JavaScript and React JS Bootcamp</h1>
+      <p>
+        {" "}
+        This was the amazing and useful Bootcamp taken by Shaurya Sinha we
+        covered everything from scratch including Javascript React.js,HTML);
+      </p>
+    </div>
+  );
+}
+export default Info;
